4e51007e4f4e382d318466ec9102503721ab5a63
Federico De Benedictis <66374898+MenneaDb@users.noreply.github.com>
2021-03-11 18:21:53 +0100

    The text views that I added to the project the are now aligned to each other because of the BASELINE CONSTRAINT feature

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // we call here the main function that handle the logic to change color while clicking on a view
+        setListener()
     }
+
+    private fun setListener(){
+        // because we set the same ClickListener on each view, we use a list of view and then set listener for each view.
+        val clickableViews: List<View> = listOf(box_one_text, box_two_text, box_three_text,
+                                                box_four_text, box_five_text, constraint_layout)
+        // set the Listener for each view
+        for (item in clickableViews){
+            item.setOnClickListener { makeColored(it)}
+        }
+    }
+
+    private fun makeColored(view: View){
+     // every view has a resource ID, we pick a color depending on which the source ID is. common pattern to use one click handler when the type of action is the same
+    when(view.id){
+        // Boxes using Color class colors for background
+        R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+        R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+        // Boxes using Android color resources for background
+        R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+        R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+        R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+        // if the user tap the background of the views it gets light gray which reveals the outlines of the other boxes to click as a hint.( to make this work we need to call the layout in question inside the listOf views)
+        else -> view.setBackgroundColor(Color.LTGRAY)
+    }
+
+
+    }
+
 }
