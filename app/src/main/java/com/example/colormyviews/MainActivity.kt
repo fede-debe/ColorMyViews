c66cc2ff5fa4d4357b78b308f553700cb989fda8
Federico De Benedictis <66374898+MenneaDb@users.noreply.github.com>
2021-03-11 19:23:10 +0100

    Three new button are now aligned by using the baseline constraint and a horizontal chain. We can use them to set different colors

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -17,7 +17,8 @@ class MainActivity : AppCompatActivity() {
     private fun setListener(){
         // because we set the same ClickListener on each view, we use a list of view and then set listener for each view.
         val clickableViews: List<View> = listOf(box_one_text, box_two_text, box_three_text,
-                                                box_four_text, box_five_text, constraint_layout)
+                                                box_four_text, box_five_text, constraint_layout,
+                                                red_button, yellow_button, green_button)
         // set the Listener for each view
         for (item in clickableViews){
             item.setOnClickListener { makeColored(it)}
@@ -35,6 +36,11 @@ class MainActivity : AppCompatActivity() {
         R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
         R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
         R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+        // Boxes using custom colors for background
+        R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+        R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+        R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
 
         // if the user tap the background of the views it gets light gray which reveals the outlines of the other boxes to click as a hint.( to make this work we need to call the layout in question inside the listOf views)
         else -> view.setBackgroundColor(Color.LTGRAY)
